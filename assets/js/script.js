0bbb36831319ac65810f3cd3b1cefa29ea0adef5
Pablo Burgos <pabloburgosc@gmail.com>
2024-03-10 22:05:28 -0300

    termina lógica de js y añade estilo css básico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
+//variables de juego
+
+let puntajePlayer = 0;
+let puntajeMaquina = 0;
+let partidasJugadas = 1;
+let empates = 0;
 let numeroPartidas = parseInt(
   prompt("Ingrese el número de partidas a jugar: ")
 );
-let puntajePlayer = 0;
-let puntajeMaquina = 0;
-let partidasJugadas = 0;
 
-function machineChoice() {
-  const choices = ["papel", "piedra", "tijera"];
-  return choices[Math.floor(Math.random() * 3)];
-}
-
-function rondaDeJuego(playerChoice, machineChoice) {
+//función que determina lógica del juego y añade marcador
+function determinaGanador(playerChoice, machineChoice) {
   if (playerChoice === machineChoice) {
+    empates++;
     return "Empate!";
   } else if (
     (playerChoice === "piedra" && machineChoice === "tijera") ||
@@ -24,4 +24,70 @@ function rondaDeJuego(playerChoice, machineChoice) {
     puntajeMaquina++;
     return "Perdiste!";
   }
+}
+
+//Función que determina jugada de la máquna
+function getMachineChoice() {
+  const choices = ["piedra", "papel", "tijera"];
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+//Función que determina resultados y actualiza los textos
+function jugarPartida(playerChoice) {
+  const machineChoice = getMachineChoice();
+  const result = determinaGanador(playerChoice, machineChoice);
+
+  document.getElementById("result").innerText = result;
+  document.getElementById("user-score").innerText = puntajePlayer;
+  document.getElementById("computer-score").innerText = puntajeMaquina;
+  document.getElementById("plays").innerText = partidasJugadas;
+  document.getElementById("ties").innerText = empates;
+
+  partidasJugadas++;
+
+  if (partidasJugadas > numeroPartidas && puntajePlayer > puntajeMaquina) {
+    document.getElementById("resultadoFinal").innerText =
+      "La partida ha terminado, has triunfado!.";
+  } else if (
+    partidasJugadas > numeroPartidas &&
+    puntajePlayer < puntajeMaquina
+  ) {
+    document.getElementById("resultadoFinal").innerText =
+      "La partida ha terminado, has perdido.";
+  } else if (
+    partidasJugadas > numeroPartidas &&
+    puntajePlayer === puntajeMaquina
+  ) {
+    document.getElementById("resultadoFinal").innerText = "Es un empate!!!";
+  }
+}
+
+//Listeners que activan los botones y determinan el final de la partida
+document.getElementById("piedra").addEventListener("click", function () {
+  jugarPartida("piedra");
+  if (partidasJugadas > numeroPartidas) {
+    terminaPartida();
+  }
+});
+
+document.getElementById("papel").addEventListener("click", function () {
+  jugarPartida("papel");
+  if (partidasJugadas > numeroPartidas) {
+    terminaPartida();
+  }
+});
+
+document.getElementById("tijera").addEventListener("click", function () {
+  jugarPartida("tijera");
+  if (partidasJugadas > numeroPartidas) {
+    terminaPartida();
+  }
+});
+
+//Función que deshabilita los botones al terminar la partida, y esconde el display de resultados
+function terminaPartida() {
+  document.getElementById("result").style.display = "none";
+  document.getElementById("piedra").disabled = true;
+  document.getElementById("papel").disabled = true;
+  document.getElementById("tijera").disabled = true;
 }
